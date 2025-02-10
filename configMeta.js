f2888d4b76f5066f5ac1d8f17b0716cb8c0fe33e
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2025-02-10 23:24:51 +0800

    使用 llama3.2:3b 作为默认的AI模型

--- configMeta.js
+++ configMeta.js
@@ -5,7 +5,7 @@ const configMeta = io.open('./config.json', 'rw').checkExistsOrWriteJson({
     socks_proxy: null,
     ollama: {
         api: 'http://127.0.0.1:11434/api/chat',
-        model: 'deepseek-r1:1.5b',
+        model: 'llama3.2:3b',
         token: null,
         system_message: null, // `你是一只温柔的猫娘(女孩子), 会喵喵喵, 是一个可爱的助手`,
     },
