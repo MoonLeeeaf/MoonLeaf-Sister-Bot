c35dff69c30035956912580360da8597c30d432c
MoonLeeeaf <150461955+MoonLeeeaf@users.noreply.github.com>
2025-02-10 18:57:44 +0800

    chore: make config readable everywhere

---
+++ configMeta.js
@@ -0,0 +1,14 @@
+import io from './lib/io.js'
+
+const configMeta = io.open('./config.json', 'rw').checkExistsOrWriteJson({
+    token: '',
+    socks_proxy: null,
+    ollama: {
+        api: 'http://127.0.0.1:11434/api/chat',
+        model: 'deepseek-r1:1.5b',
+        token: null,
+        system_message: null, // `你是一只温柔的猫娘(女孩子), 会喵喵喵, 是一个可爱的助手`,
+    },
+}).readAllJsonAndClose()
+
+export default configMeta
